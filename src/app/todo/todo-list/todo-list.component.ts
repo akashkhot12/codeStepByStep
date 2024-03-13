6b3e79891e40206f2ba969c4d74573169d402bfe
akashkhot12 <akashkhot03@gmail.com>
2024-03-13 16:27:15 +0530

    complete to do list app

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class TodoListComponent {
 
+  list:any[] = []
+  addData(item:string){
+    this.list.push({id:this.list.length,name:item})
+  }
+
+  removeData(id:number){
+    this.list = this.list.filter(item=>item.id!==id)
+  }
 }
