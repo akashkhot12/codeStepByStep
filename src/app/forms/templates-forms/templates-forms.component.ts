984aa9c51fc4563393976981366375d3c9491870
akashkhot12 <akashkhot03@gmail.com>
2024-03-18 09:55:30 +0530

    adding templates form data into console

--- src/app/forms/templates-forms/templates-forms.component.ts
+++ src/app/forms/templates-forms/templates-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Form } from '@angular/forms';
 
 @Component({
   selector: 'app-templates-forms',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './templates-forms.component.css'
 })
 export class TemplatesFormsComponent {
+
+  loginForm(data:Form){
+    console.log(data);
+    
+  }
 
 }
